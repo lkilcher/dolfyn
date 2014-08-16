8b2ff8d728e9eaa7c2f77a338ee7c528ac7af87c
Levi Kilcher <Levi.Kilcher@nrel.gov>
2014-08-15 18:42:07 -0600

    More pip fixes.

--- setup.py
+++ setup.py
@@ -1,37 +1,6 @@
 # The setup script for installing DOLfYN.
-from numpy.distutils.core import setup,Extension,Command
-from numpy.distutils.command.install_data import install_data
-from distutils.command.install import install
-from distutils import log
+from distutils.core import setup
 import dolfyn._version as ver
-import os
-
-class chmod_install_data(install_data):
-    """
-    This class ensures that data files get installed with
-    read permissions.
-    """
-    def run(self):
-        install_data.run(self)
-        if os.name in ['posix']:
-            for fn in self.get_outputs():
-                if not (fn.endswith('.py') or fn.endswith('.pyc')):
-                    mode = (( (os.stat(fn).st_mode) | 0o554 ) & 0o7777 )
-                    log.info(('changing mode of %s to %o' % (fn, mode)))
-                    os.chmod(fn,mode)
-
-class chmod_install(install):
-    """
-    This class ensures that scripts and extensions get installed with
-    read and execute permissions.
-    """
-    def run(self):
-        install.run(self)
-        if os.name in ['posix']:
-            for fn in self.get_outputs():
-                mode = (( (os.stat(fn).st_mode) | 0o555 ) & 0o7777 )
-                log.info(('changing mode of %s to %o' % (fn, mode)))
-                os.chmod(fn,mode)
 
 setup(name='dolfyn',
       version=ver.__version__,
