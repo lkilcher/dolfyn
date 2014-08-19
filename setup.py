12a5172583d203371530bfb37ca296f16c74b614
Levi Kilcher <Levi.Kilcher@nrel.gov>
2014-08-18 18:46:25 -0600

    Moved from distutils to setuptools.

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 # The setup script for installing DOLfYN.
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 import dolfyn._version as ver
 
-setup(name='dolfyn',
-      version=ver.__version__,
-      description='Doppler Ocean Library for pYthoN.',
-      author='Levi Kilcher',
-      author_email='levi.kilcher@nrel.gov',
+setup(name = 'dolfyn',
+      version = ver.__version__,
+      description = 'Doppler Ocean Library for pYthoN.',
+      author = 'Levi Kilcher',
+      author_email = 'levi.kilcher@nrel.gov',
       classifiers = ['Development Status :: 3 - Alpha',
                      'Intended Audience :: Science/Research',
                      'License :: OSI Approved :: Apache Software License',
                      'Natural Language :: English',
                      #'Topic :: Scientific/Engineering :: Earth Science',
                      ],
-      url='http://github.com/lkilcher/dolfyn',
-      #download_url = 'https://github.com/lkilcher/dolfyn/tarball/0.2.beta.2',
-      packages=['dolfyn','dolfyn.adv','dolfyn.io','dolfyn.data','dolfyn.meta','dolfyn.tools','dolfyn.adcp',],
-      install_requires = ['numpy', 'h5py', 'scipy', 'matplotlib'],
-      provides=['dolfyn',],
-      scripts=['scripts/motcorrect_vector.py','scripts/vec2mat.py'],
-      #cmdclass={'install_data':chmod_install_data,'install':chmod_install,},
+      url = 'http://github.com/lkilcher/dolfyn',
+      #download_url = 'https://github.com/lkilcher/dolfyn/tarball/0.2.beta.3',
+      packages = ['dolfyn','dolfyn.adv','dolfyn.io','dolfyn.data','dolfyn.meta','dolfyn.tools','dolfyn.adcp',],
+      install_requires = ['numpy', 'h5py', 'scipy', ],
+      provides = ['dolfyn',],
+      scripts = ['scripts/motcorrect_vector.py','scripts/vec2mat.py'],
+      #entry_points = {
+      #    'console_scripts':
+      #    ['motcorrect_vector = dolfyn.adv.scripts:motcorrect_vector',
+      #     ],
+      #    },
+      dependency_links = ['https://pypi.python.org/pypi'],
+      #cmdclass = {'install_data':chmod_install_data,'install':chmod_install,},
       )
