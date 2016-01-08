f29c24d452c2ec7df6d37130b79be3f0de74903c
Levi Kilcher <Levi.Kilcher@nrel.gov>
2016-01-08 15:14:51 -0700

    Update and simplify tests.

--- test/adv_tests.py
+++ test/adv_tests.py
@@ -2,6 +2,8 @@ import dolfyn.adv.api as avm
 import dolfyn.data.base
 import numpy as np
 from os import path
+import pycoda as pcd
+
 
 dolfyn.data.base.debug_level = 1
 
@@ -15,14 +17,13 @@ pkg_root = test_root.rsplit('/', 2)[0] + "/"
 dat = avm.load(test_root + 'data/vector_data01.h5', 'ALL')
 dat_imu = avm.load(test_root + 'data/vector_data_imu01.h5', 'ALL')
 
-print 1
 
 def read_test(make_data=False):
 
     td = avm.read_nortek(pkg_root + 'example_data/vector_data01.VEC')
 
     if make_data:
-        td.save(test_root + 'data/vector_data01.h5')
+        td.to_hdf5(test_root + 'data/vector_data01.h5')
         return
 
     err_str = ("The output of read_nortek('vector_data01.VEC') "
@@ -39,7 +40,7 @@ def read_test_imu(make_data=False):
     td.props['body2head_vec'] = np.array([-1.0, 0.5, 0.2])
 
     if make_data:
-        td.save(test_root + 'data/vector_data_imu01.h5')
+        td.to_hdf5(test_root + 'data/vector_data_imu01.h5')
         return
 
     err_str = ("The output of read_nortek('vector_data_imu01.VEC') "
@@ -48,16 +49,33 @@ def read_test_imu(make_data=False):
 
 
 # For non-IMU advs:
-#def inst2earth_test(make_data=False):
+def inst2earth_test(make_data=False):
+    tdm = dat.copy()
+    avm.rotate.inst2earth(tdm)
+    # only compare the 'vel' data.
+    tdm = pcd.data(vel=tdm['vel'])
+
+    if make_data:
+        tdm.to_hdf5(test_root + 'data/vector_data_imu01_inst2earth.h5')
+        return
+
+    cdm = avm.load(test_root + 'data/vector_data_imu01_inst2earth.h5', 'ALL')
+
+    assert tdm == cdm, "Motion correction does not match expectations."
 
 #def earth2principal_test(make_data=False):
+
+#def subset_test(make_data=False):
+
+#def declination_test(make_data=False):
+
 
 def motion_test(make_data=False):
     tdm = dat_imu.copy()
     avm.motion.correct_motion(tdm)
 
     if make_data:
-        tdm.save(test_root + 'data/vector_data_imu01_mc.h5')
+        tdm.to_hdf5(test_root + 'data/vector_data_imu01_mc.h5')
         return
 
     cdm = avm.load(test_root + 'data/vector_data_imu01_mc.h5', 'ALL')
@@ -70,9 +88,10 @@ def heading_test(make_data=False):
 
     o = td['orient']
     o['pitch'], o['roll'], o['heading'] = avm.rotate.orient2euler(td)
+    td = o
 
     if make_data:
-        td.save(test_root + 'data/vector_data_imu01_head_pitch_roll.h5')
+        td.to_hdf5(test_root + 'data/vector_data_imu01_head_pitch_roll.h5')
         return
 
     cd = avm.load(test_root + 'data/vector_data_imu01_head_pitch_roll.h5', 'ALL')
@@ -82,9 +101,9 @@ def heading_test(make_data=False):
 
 def turbulence_test(make_data=False):
     td = avm.calc_turbulence(dat.copy(), 4096)
-    
+
     if make_data:
-        td.save(test_root + 'data/vector_data01_bin.h5')
+        td.to_hdf5(test_root + 'data/vector_data01_bin.h5')
         return
 
     cd = avm.load(test_root + 'data/vector_data01_bin.h5', 'ALL')
@@ -95,9 +114,11 @@ def turbulence_test(make_data=False):
 def clean_test(make_data=False):
     td = dat.copy()
     avm.clean.GN2002(td['vel'][0])
+    # only compare the 'vel' data.
+    td = pcd.data(vel=td['vel'])
 
     if make_data:
-        td.save(test_root + 'data/vector_data01_uclean.h5')
+        td.to_hdf5(test_root + 'data/vector_data01_uclean.h5')
         return
 
     cd = avm.load(test_root + 'data/vector_data01_uclean.h5', 'ALL')
--- test/make_data.py
+++ test/make_data.py
@@ -1,6 +1,6 @@
 """
 !!!Warning!!! the lines in this file will rebuild data files that
-are benchmark tests. Uncomment lines to rebuiled data files only if
+are benchmark tests. Uncomment lines to rebuild data files only if
 you know you have fixed a bug, and wish to fix an existing test data
 set.
 """
@@ -9,7 +9,9 @@ print __doc__
 import adv_tests as advt
 
 #advt.read_test(make_data=True)
+#advt.read_test_imu(make_data=True)
 #advt.motion_test(make_data=True)
 #advt.heading_test(make_data=True)
+#advt.inst2earth_test(make_data=True)
 #advt.turbulence_test(make_data=True)
 #advt.clean_test(make_data=True)
