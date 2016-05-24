8a2c0333c9a5204548a844e7694803077b64c7f1
Levi Kilcher <Levi.Kilcher@nrel.gov>
2016-05-23 21:29:11 -0600

    Prevent the creation of empty data objects.

--- dolfyn/_version.py
+++ dolfyn/_version.py
@@ -1,3 +1,3 @@
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 __prog_name__ = 'DOLfYN'
 __version_date__ = 'May-23-2016'
--- dolfyn/data/base.py
+++ dolfyn/data/base.py
@@ -368,6 +368,8 @@ class Dgroups(Dbase):
                      dt.shape[-1] == self.shape[-1])):
                     dt = dt[..., inds]
                 out.add_data(nm, dt, group=grp)
+                if np.any(np.array(dt.shape) == 0):
+                    raise IndexError('The indexing object yields empty arrays.')
         return out
 
     def del_data(self, *args):
