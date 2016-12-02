85076e960551918dce83e64dc6eb3a3ad445f06e
Levi Kilcher <Levi.Kilcher@nrel.gov>
2016-12-02 12:48:02 -0700

    Add tests for ADP files.

--- test/make_data.py
+++ test/make_data.py
@@ -7,10 +7,18 @@ set.
 print(__doc__)
 
 import adv_tests as advt
+import adp_tests as adpt
 
-# advt.rungen(advt.read_test(make_data=True))
-# advt.rungen(advt.motion_test(make_data=True))
+
+def rungen(gen):
+    for g in gen:
+        pass
+
+# rungen(advt.read_test(make_data=True))
+# rungen(advt.motion_test(make_data=True))
 # advt.heading_test(make_data=True)
 # advt.turbulence_test(make_data=True)
 # advt.clean_test(make_data=True)
-# advt.rungen(advt.subset_test(make_data=True))
+# rungen(advt.subset_test(make_data=True))
+
+# rungen(adpt.read_test(make_data=True))
