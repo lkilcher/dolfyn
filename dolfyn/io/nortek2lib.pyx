9c04c93e98d044fa5d1c3aed72c5ad563eee369b
Levi Kilcher <Levi.Kilcher@gmail.com>
2017-12-17 09:42:31 -0700

    Fix the ensemble count again.
    This was removed while adding other checks, but now it needs to come
    back.

--- dolfyn/io/nortek2lib.pyx
+++ dolfyn/io/nortek2lib.pyx
@@ -8,10 +8,23 @@ cimport numpy as np
 
 
 cdef struct Index:
-    np.uint32_t N
+    np.uint64_t N
+    np.uint64_t pos
     np.uint16_t ID # This is uint8 in the data, but we need this struct to be a multiple of 64 bits, so it's padded here
     np.uint16_t config
-    np.uint64_t pos
+    np.uint16_t beams_cy
+    np.uint16_t _blank
+
+# This must match above
+index_dtype = np.dtype([('ens', np.uint64),
+                        ('pos', np.uint64),
+                        ('ID', np.uint16),
+                        ('config', np.uint16),
+                        ('beams_cy', np.uint16),
+                        ('_blank', np.uint16),
+])
+
+
 
 hdr = struct.Struct('<BBBBhhh')
 
@@ -20,9 +33,9 @@ def create_index_slow(infile, outfile, N_ens):
     fin = open(infile, 'rb')
     fout = open(outfile, 'wb')
     ens = 0
-    last_ens = None
     N = 0
     config = 0
+    last_ens = 1
     while N < N_ens:
         pos = fin.tell()
         try:
@@ -32,17 +45,19 @@ def create_index_slow(infile, outfile, N_ens):
         if dat[2] in [21, 24]:
             fin.seek(2, 1)
             config = struct.unpack('<H', fin.read(2))[0]
-            fin.seek(68, 1)
+            fin.seek(26, 1)
+            beams_cy = struct.unpack('<H', fin.read(2))[0]
+            fin.seek(40, 1)
             ens = struct.unpack('<I', fin.read(4))[0]
             if last_ens != ens:
-                #print(N, ens, config, pos)
-                fout.write(struct.pack('<LHHQ', N, dat[2], config, pos))
                 N += 1
+            fout.write(struct.pack('<QQ4H', N, pos, dat[2], config, beams_cy, 0))
             fin.seek(dat[4] - 76, 1)
+            last_ens = ens
         else:
             fin.seek(dat[4], 1)
-        last_ens = ens
-        #print('%10d: %02X, %d, %02X, %d\n' % (pos, dat[0], dat[1], dat[2], dat[4]))
+        # if N < 5:
+        #     print('%10d: %02X, %d, %02X, %d, %d, %d, %d\n' % (pos, dat[0], dat[1], dat[2], dat[4], N, ens, last_ens))
     fin.close()
     fout.close()
 
@@ -51,14 +66,14 @@ cdef create_index(str infile, str outfile, long N_ens):
     cdef FILE *fin = fopen(infile, "rb")
     cdef FILE *fout = fopen(outfile, "wb")
     cdef lib.Header hd
-    cdef np.uint32_t ens, last_ens, retval, ensemble_pos
+    cdef np.uint32_t ens, retval, last_ens
     cdef Index idx
     #cdef lib.BurstHead bhead
-    ensemble_pos = 72
     idx.N = 0
     idx.ID = 0
     idx.config = 0
-    last_ens = 0
+    idx._blank = 0
+    last_ens = 1
     while idx.N < N_ens:
         idx.pos = ftell(fin)
         retval = fread(&hd, sizeof(lib.Header), 1, fin)
@@ -68,21 +83,21 @@ cdef create_index(str infile, str outfile, long N_ens):
             break
         if hd.ID in [21, 24]:
             idx.ID = hd.ID
-            fseek(fin, 2, SEEK_CUR)
-            fread(&idx.config, sizeof(idx.config), 1, fin)
-            # fread(&bhead, sizeof(bhead), 1, fin)
-            # fseek(fin, -sizeof(bhead), SEEK_CUR)
-            #printf("ver: %02d, off: %02d, year: %04d, ens: %05d, longsize: %d, ret: %d\n", bhead.ver, bhead.DataOffset, bhead.year + 1900, bhead.ens, sizeof(ens), retval)
-            # Scan ahead 72 bytes to where the ensemble count is.
-            fseek(fin, ensemble_pos - 4, SEEK_CUR)
-            fread(&ens, sizeof(ens), 1, fin)
+            fseek(fin, 2, SEEK_CUR) # 2 bytes (pos: 2)
+            fread(&idx.config, sizeof(idx.config), 1, fin) # 2 bytes (pos: 4)
+            fseek(fin, 26, SEEK_CUR) # 26 (pos: 30)
+            fread(&idx.beams_cy, sizeof(idx.beams_cy), 1, fin) # 2 bytes (pos: 32)
+            fseek(fin, 40, SEEK_CUR) # 40 (pos: 72)
+            fread(&ens, sizeof(ens), 1, fin) # 4 bytes (pos: 76)
+            if last_ens != ens:
+                idx.N += 1
             fwrite(&idx, sizeof(idx), 1, fout)
-            idx.N += 1
-            fseek(fin, hd.dataSize - ensemble_pos - sizeof(ens), SEEK_CUR)
+            fseek(fin, hd.dataSize - 76, SEEK_CUR)
+            last_ens = ens
         else:
             fseek(fin, hd.dataSize, SEEK_CUR)
-        #printf('%10ld: %02X, %d, %02X, %d, %05u\n', idx.pos, hd.sync, hd.hdrSize, hd.ID, hd.dataSize, ens)
-        last_ens = ens
+        # if idx.N < 5:
+        #     printf('%10ld: %02X, %d, %02X, %d, %05u, %05u\n', idx.pos, hd.sync, hd.hdrSize, hd.ID, hd.dataSize, ens, last_ens)
     fclose(fin)
     fclose(fout)
 
@@ -93,12 +108,13 @@ cpdef get_index(infile, reload=False):
         print("Indexing...", end='')
         if reload == 'slow':
             create_index_slow(infile, index_file, 2 ** 32)
-        create_index(infile, index_file, 2 ** 32)
+        else:
+            create_index(infile, index_file, 2 ** 32)
         print(" Done.")
     else:
         print("Using saved index file.")
     #return np.fromfile(index_file, dtype=np.dtype([('ens', '>u4'), ('pos', '>u8')]))
-    return np.fromfile(index_file, dtype=np.dtype([('ens', '<u4'), ('ID', '<u2'), ('config', '<u2'), ('pos', '<u8')]))
+    return np.fromfile(index_file, dtype=index_dtype)
     #return np.fromfile(index_file, dtype=np.uint32 ).reshape((-1, 2))
 
 
