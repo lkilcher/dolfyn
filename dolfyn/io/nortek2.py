80a561264a9371384b16dd7e20ad606ed3a788c7
Levi Kilcher <Levi.Kilcher@gmail.com>
2017-12-19 13:47:03 -0700

    cross-walk for ID=24 (beam 5).

--- dolfyn/io/nortek2.py
+++ dolfyn/io/nortek2.py
@@ -150,6 +150,57 @@ class Ad2cpReader(object):
                 for ky in keys:
                     if ky in outdat:
                         outdat.groups.add(ky, grp)
+
+        if 24 in dat:
+            tag = '_b5'
+            dnow = dat[21]
+            cfg['burst_config' + tag] = lib.headconfig_int2dict(
+                lib.collapse(dnow['config']))
+            outdat['mpltime' + tag] = lib.calc_time(
+                dnow['year'] + 1900,
+                dnow['month'],
+                dnow['day'],
+                dnow['hour'],
+                dnow['minute'],
+                dnow['second'],
+                dnow['usec100'].astype('uint32') * 100)
+            outdat.groups.add('mpltime', '_essential')
+            tmp = lib.beams_cy_int2dict(
+                lib.collapse(dnow['beam_config']), 21)
+            cfg['ncells' + tag] = tmp['ncells']
+            cfg['coord_sys' + tag] = tmp['cy']
+            cfg['nbeams' + tag] = tmp['nbeams']
+            for ky in ['SerialNum', 'cell_size', 'blanking',
+                       'nom_corr', 'data_desc',
+                       'vel_scale', 'power_level']:
+                cfg[ky + tag] = lib.collapse(dnow[ky])
+            for ky in ['c_sound', 'temp', 'press',
+                       'heading', 'pitch', 'roll',
+                       'temp_press', 'batt_V',
+                       'temp_mag', 'temp_clock',
+                       'Mag', 'Acc',
+                       'ambig_vel', 'xmit_energy',
+                       'error', 'status0', 'status', 'ensemble']:
+                # No if statement here
+                outdat[ky + tag] = dnow[ky]
+            for ky in [
+                    'vel', 'amp', 'corr',
+                    'alt_dist', 'alt_quality', 'alt_status',
+                    'ast_dist', 'ast_quality', 'ast_offset_time',
+                    'ast_pressure',
+                    'altraw_nsamp', 'altraw_dist', 'altraw_samp',
+                    'echo',
+                    'orientmat', 'ahrs_gyro',
+                    'percent_good',
+                    'std_pitch', 'std_roll', 'std_heading', 'std_press'
+            ]:
+                if ky in dnow:
+                    outdat[ky + tag] = dnow[ky]
+            for grp, keys in defs._burst_group_org.items():
+                for ky in keys:
+                    if ky + tag in outdat:
+                        outdat.groups.add(ky + tag, grp)
+        outdat.props['coord_sys'] = cfg['coord_sys']
         return outdat
 
     def __exit__(self, type, value, trace,):
