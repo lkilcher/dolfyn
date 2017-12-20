e6b62abcb3cce550a7bc3721cb0c7865a3909c7f
Levi Kilcher <Levi.Kilcher@gmail.com>
2017-12-19 17:55:00 -0700

    Update test-script.

--- test_ad2cp.py
+++ test_ad2cp.py
@@ -1,19 +1,23 @@
 import dolfyn.io.nortek2 as nrtk
 import os.path as path
+import warnings
 reload(nrtk)
+
+# warnings.filterwarnings('error')
 
 testfile = path.expanduser('~/data/WA2017/SMB500_Signature1000_Jul2017/'
                            'SMB500_Sig1000_Jul2017.ad2cp')
 
 
-def readfile(nens):
+def readfile(e_start=0, e_stop=None):
     rdr = nrtk.Ad2cpReader(testfile)
-    d = rdr.readfile(0, nens)
+    d = rdr.readfile(e_start, e_stop)
     rdr.sci_data(d)
     out = nrtk.reorg(d)
     nrtk.reduce(out)
     return out, d
 
 
-dat, d = readfile(None)
+#dat, d = readfile(0, 1000)
+dat, d = readfile(0, None)
 dat.save('/Users/lkilcher/tmp/test.h5')
