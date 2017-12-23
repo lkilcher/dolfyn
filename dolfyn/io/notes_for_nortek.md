972fa2eb89e6a8fc3b9a4012601e8bfceb62ea9c
Levi Kilcher <Levi.Kilcher@gmail.com>
2017-12-22 19:39:40 -0700

    Some notes, and fixes.

---
+++ dolfyn/io/notes_for_nortek.md
@@ -0,0 +1,2 @@
+1. Why does ensemble stop at 4096 (2**12)? (or 3600?)
+2. Is the max file-size documented? I maxed-out at 23.25GB, then the file wrapped!
--- test_ad2cp.py
+++ test_ad2cp.py
@@ -8,14 +8,17 @@ reload(nrtk)
 
 testfile = path.expanduser('~/data/WA2017/SMB500_Signature1000_Jul2017/'
                            'SMB500_Sig1000_Jul2017.ad2cp')
-# testfile = path.expanduser('~/data/wp2017/adp/signature/'
-#                            'S100259A012_WPdeploy.ad2cp')
+testfile = path.expanduser('~/data/wp2017/adp/signature/'
+                           'S100259A012_WPdeploy.ad2cp')
 
 
 #idx = lib.get_index(testfile, reload=True)
-#idx = lib.get_index(testfile)
+idx = lib.get_index(testfile)
+
+# dat = nrtk.read_signature(testfile, 10195200, 10368000)
+dat = nrtk.read_signature(testfile, 10368000, 10540800)
 
 #dat = nrtk.read_signature(testfile, 0, 10000)
-nrtk.split_to_hdf(testfile, 100000)
+#nrtk.split_to_hdf(testfile, 100000)
 #dat, d = readfile(0, None)
 #dat.save('/Users/lkilcher/tmp/test.h5')
