4d4f7e3fc27588fea1c2f504486cced7cd9ddd55
Levi Kilcher <levi.kilcher@gmail.com>
2018-06-04 16:52:45 -0600

    Use centralized functions for load/read in tests.

--- dolfyn/test/base.py
+++ dolfyn/test/base.py
@@ -1,6 +1,8 @@
 import pkg_resources
 import atexit
 import pyDictH5.base as pb
+from dolfyn.io.hdf5 import load as _load
+import numpy as np
 
 pb.debug_level = 10
 
@@ -12,10 +14,31 @@ def rungen(gen):
         pass
 
 
+def data_equiv(dat1, dat2, message=''):
+    assert dat1 == dat2, message
+
+
+def assert_close(dat1, dat2, message='', *args):
+    assert np.allclose(dat1, dat2, *args), message
+
+
 class ResourceFilename(object):
 
-    def __init__(self, package_or_requirement):
+    def __init__(self, package_or_requirement, prefix=''):
         self.pkg = package_or_requirement
+        self.prefix = prefix
 
     def __call__(self, name):
-        return pkg_resources.resource_filename(self.pkg, name)
+        return pkg_resources.resource_filename(self.pkg, self.prefix + name)
+
+
+rfnm = ResourceFilename('dolfyn.test', prefix='data/')
+exdt = ResourceFilename('dolfyn', prefix='example_data/')
+
+
+def load_tdata(name, *args, **kwargs):
+    return _load(rfnm(name), *args, **kwargs)
+
+
+def save_tdata(data, name, *args, **kwargs):
+    data.to_hdf5(rfnm(name), *args, **kwargs)
--- dolfyn/test/test_rotate_adp.py
+++ dolfyn/test/test_rotate_adp.py
@@ -1,8 +1,5 @@
 from dolfyn.test import test_read_adp as tr
-from dolfyn.io.hdf5 import load
-
-rfnm = tr.rfnm
-data_equiv = tr.data_equiv
+from .base import load_tdata as load, save_tdata as save, data_equiv
 
 
 def test_rotate_beam2inst(make_data=False):
@@ -12,13 +9,13 @@ def test_rotate_beam2inst(make_data=False):
     td_sigi = tr.dat_sigi.rotate2('inst')
 
     if make_data:
-        td.to_hdf5(rfnm('data/RDI_test01_rotate_beam2inst.h5'))
-        td_sig.to_hdf5(rfnm('data/BenchFile01_rotate_beam2inst.h5'))
-        td_sigi.to_hdf5(rfnm('data/Sig1000_IMU_rotate_beam2inst.h5'))
+        save(td, 'RDI_test01_rotate_beam2inst.h5')
+        save(td_sig, 'BenchFile01_rotate_beam2inst.h5')
+        save(td_sigi, 'Sig1000_IMU_rotate_beam2inst.h5')
         return
 
-    cd_sig = load(rfnm('data/BenchFile01_rotate_beam2inst.h5'))
-    cd_sigi = load(rfnm('data/Sig1000_IMU_rotate_beam2inst.h5'))
+    cd_sig = load('BenchFile01_rotate_beam2inst.h5')
+    cd_sigi = load('Sig1000_IMU_rotate_beam2inst.h5')
 
     msg = "adp.rotate.beam2inst gives unexpected results for {}"
     for t, c, msg in (
@@ -33,16 +30,16 @@ def test_rotate_inst2beam(make_data=False):
 
     # # The reverse RDI rotation doesn't work b/c of NaN's in one beam
     # # that propagate to others.
-    # td = load(rfnm('data/RDI_test01_rotate_beam2inst.h5'))
+    # td = load('RDI_test01_rotate_beam2inst.h5')
     # td.rotate('beam', inplace=True)
-    td_awac = load(rfnm('data/AWAC_test01_earth2inst.h5'))
+    td_awac = load('AWAC_test01_earth2inst.h5')
     td_awac.rotate2('beam', inplace=True)
 
     if make_data:
-        td_awac.to_hdf5(rfnm('data/AWAC_test01_inst2beam.h5'))
+        save(td_awac, 'AWAC_test01_inst2beam.h5')
         return
 
-    cd_awac = load(rfnm('data/AWAC_test01_inst2beam.h5'))
+    cd_awac = load('AWAC_test01_inst2beam.h5')
 
     msg = "adp.rotate.beam2inst gives unexpected REVERSE results for {}"
     for t, c, msg in (
@@ -53,24 +50,24 @@ def test_rotate_inst2beam(make_data=False):
 
 
 def test_rotate_earth2inst(make_data=False):
-    td = (load(rfnm('data/RDI_test01_rotate_inst2earth.h5'))
+    td = (load('RDI_test01_rotate_inst2earth.h5')
           .rotate2('inst', inplace=True))
-    # tdwr2 = load(rfnm('data/winriver02_rotate_ship2earth.h5'))
+    # tdwr2 = load('winriver02_rotate_ship2earth.h5')
     # tdwr2.rotate2('inst', inplace=True)
     # This AWAC is in earth coords.
     td_awac = tr.dat_awac.rotate2('inst')
-    td_sig = load(rfnm('data/BenchFile01_rotate_inst2earth.h5'))
+    td_sig = load('BenchFile01_rotate_inst2earth.h5')
     td_sig.rotate2('inst', inplace=True)
-    td_sigi = load(rfnm('data/Sig1000_IMU_rotate_inst2earth.h5'))
+    td_sigi = load('Sig1000_IMU_rotate_inst2earth.h5')
     td_sigi.rotate2('inst', inplace=True)
 
     if make_data:
-        td_awac.to_hdf5(rfnm('data/AWAC_test01_earth2inst.h5'))
+        save(td_awac, 'AWAC_test01_earth2inst.h5')
         return
 
-    cd_awac = load(rfnm('data/AWAC_test01_earth2inst.h5'))
-    cd_sig = load(rfnm('data/BenchFile01_rotate_beam2inst.h5'))
-    cd_sigi = load(rfnm('data/Sig1000_IMU_rotate_beam2inst.h5'))
+    cd_awac = load('AWAC_test01_earth2inst.h5')
+    cd_sig = load('BenchFile01_rotate_beam2inst.h5')
+    cd_sigi = load('Sig1000_IMU_rotate_beam2inst.h5')
 
     msg = "adp.rotate.inst2earth gives unexpected REVERSE results for {}"
     for t, c, msg in (
@@ -87,24 +84,24 @@ def test_rotate_inst2earth(make_data=False):
 
     td = tr.dat_rdi_i.rotate2('earth')
     tdwr2 = tr.dat_wr2.rotate2('earth')
-    td_sig = load(rfnm('data/BenchFile01_rotate_beam2inst.h5'))
+    td_sig = load('BenchFile01_rotate_beam2inst.h5')
     td_sig.rotate2('earth', inplace=True)
-    td_sigi = load(rfnm('data/Sig1000_IMU_rotate_beam2inst.h5'))
+    td_sigi = load('Sig1000_IMU_rotate_beam2inst.h5')
     td_sigi.rotate2('earth', inplace=True)
-    td_awac = load(rfnm('data/AWAC_test01_earth2inst.h5'))
+    td_awac = load('AWAC_test01_earth2inst.h5')
     td_awac.rotate2('earth', inplace=True)
 
     if make_data:
-        td.to_hdf5(rfnm('data/RDI_test01_rotate_inst2earth.h5'))
-        td_sig.to_hdf5(rfnm('data/BenchFile01_rotate_inst2earth.h5'))
-        td_sigi.to_hdf5(rfnm('data/Sig1000_IMU_rotate_inst2earth.h5'))
-        tdwr2.to_hdf5(rfnm('data/winriver02_rotate_ship2earth.h5'))
+        save(td, 'RDI_test01_rotate_inst2earth.h5')
+        save(td_sig, 'BenchFile01_rotate_inst2earth.h5')
+        save(td_sigi, 'Sig1000_IMU_rotate_inst2earth.h5')
+        save(tdwr2, 'winriver02_rotate_ship2earth.h5')
         return
 
-    cd = load(rfnm('data/RDI_test01_rotate_inst2earth.h5'))
-    cdwr2 = load(rfnm('data/winriver02_rotate_ship2earth.h5'))
-    cd_sig = load(rfnm('data/BenchFile01_rotate_inst2earth.h5'))
-    cd_sigi = load(rfnm('data/Sig1000_IMU_rotate_inst2earth.h5'))
+    cd = load('RDI_test01_rotate_inst2earth.h5')
+    cdwr2 = load('winriver02_rotate_ship2earth.h5')
+    cd_sig = load('BenchFile01_rotate_inst2earth.h5')
+    cd_sigi = load('Sig1000_IMU_rotate_inst2earth.h5')
     cd_awac = tr.dat_awac
 
     msg = "adp.rotate.inst2earth gives unexpected results for {}"
