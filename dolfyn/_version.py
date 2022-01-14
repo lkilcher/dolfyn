434f56d1ceb32e7841e4c13170bec72bd6f1ac6c
Levi Kilcher <levi.kilcher@gmail.com>
2022-01-14 09:15:34 -0700

    Change version to 1.0

--- dolfyn/_version.py
+++ dolfyn/_version.py
@@ -1,14 +1,21 @@
-__version__ = '0.13.1'
+__version__ = '1.0.0-rc1'
 __prog_name__ = 'DOLfYN'
 __version_date__ = 'Aug-26-2021'
 
 
 def ver2tuple(ver):
     out = []
+    if '-' in __version__:
+        ver, pre_rel = ver.split('-')
+    else:
+        pre_rel = None
     for val in ver.split('.'):
         val = int(val)
         out.append(val)
-    return tuple(out)
+    if pre_rel is not None:
+        return tuple(out + [pre_rel])
+    else:
+        return tuple(out)
 
 
 version_info = ver2tuple(__version__)
