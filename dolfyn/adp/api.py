008759eef28e85d543d7f452d8bd997d553a184b
jmcvey3 <53623232+jmcvey3@users.noreply.github.com>
2022-12-05 09:09:07 -0800

    Keep API simple

--- dolfyn/adp/api.py
+++ dolfyn/adp/api.py
@@ -2,4 +2,4 @@ from ..io.api import read, load
 from ..rotate.api import rotate2
 from . import clean
 from ..velocity import VelBinner
-from .turbulence import ADPBinner, calc_turbulence
+from .turbulence import ADPBinner
